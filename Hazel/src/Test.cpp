d765e6325a44c0585de30b945b79ae45c06b94ea
Matthew Whalen <mrwhalen22@gmail.com>
2022-01-04 22:56:10 -0500

    Setup VS project

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+
+#include <stdio.h>
+
+namespace Hazel {
+	void print() { 
+		printf("Hello World");
+	}
+
+}
---
+++ Sandbox/src/application.cpp
@@ -0,0 +1,8 @@
+
+namespace Hazel {
+	__declspec(dllexport) void print();
+}
+int main() {
+	Hazel::print();
+
+}
